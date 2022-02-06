fb902c6f9c5ab5b7bae20daf0fc33c6af5598ceb
root <root@ITIC_ADMIN.mav.local>
2022-02-06 19:17:44 +0300

    test setTimeout and setInterval

--- testjs_workspase/main.js
+++ testjs_workspase/main.js
@@ -1,6 +1,29 @@
 
 
 
-const elem = document.querySelector('.some-div');
+let timeId = setTimeout(function tick() {
+  alert('tick');
+  timeId = setTimeout(tick, 2000)
+}, 2000);
 
-console.log(elem.offsetHeight);
+// setTimeout
+const test = () => clearTimeout(timeId);
+
+setTimeout(test, 10000);
+
+// (clearTimeout(timeId), 10000);
+
+// setTimeout(function timeout() {
+//   console.log('Таймаут');
+// }, 0);
+
+// let p = new Promise(function(resolve, reject) {
+//   console.log('Создание промиса');
+//   resolve();
+// });
+
+// p.then(function() {
+//   console.log('Обработка промиса');
+// });
+
+// console.log('конец скрипта');
