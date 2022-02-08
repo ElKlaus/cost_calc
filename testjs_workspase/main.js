30be414f152cd09fe1d908072a9d6ee525a0effa
ElKlaus <klaus15rus@gmail.com>
2022-02-08 18:54:13 +0300

    Learn binary search

--- testjs_workspase/main.js
+++ testjs_workspase/main.js
@@ -1,15 +1,45 @@
+const binarySearch = (list, item) => {
+  let low = 0;
+  let high = list.length - 1;
+
+  while (low <= high) {
+    let mid = (low + high);
+    let guess = list[mid];
+
+    if ( guess === item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1
+    }
+
+    console.log(mid, 'adfa');
+  };
+
+  
+};
 
 
+const someArr = [1, 3, 5, 7, 9];
 
-let timeId = setTimeout(function tick() {
-  alert('tick');
-  timeId = setTimeout(tick, 2000)
-}, 2000);
 
-// setTimeout
-const test = () => clearTimeout(timeId);
+binarySearch(someArr, 9);
 
-setTimeout(test, 10000);
+// const user = { name: 'Vasya', married: true, age: 25};
+// const name = 'age';
+// user[name] = 'Inna';
+
+
+// let timeId = setTimeout(function tick() {
+//   alert('tick');
+//   timeId = setTimeout(tick, 2000)
+// }, 2000);
+
+// // setTimeout
+// const test = () => clearTimeout(timeId);
+
+// setTimeout(test, 10000);
 
 // (clearTimeout(timeId), 10000);
 
