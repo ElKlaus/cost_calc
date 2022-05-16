c2df02127902e2b5be177e774222ccb4c5a455aa
ElKlaus <klaus15rus@gmail.com>
2022-05-16 11:09:33 +0300

    Learn binary search

--- testjs_workspace/main.js
+++ testjs_workspace/main.js
@@ -1,5 +1,28 @@
+const binarySearch = (list, item) => {
+  let low = 0;
+  let high = list.length - 1;
 
+  while (low <= high) {
+    let mid = low + high;
+    let guess = list[mid];
+    
+    if (guess === item) {
+      return mid;
+    } else if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
 
+  return null;
+};
+
+const myList = [1, 3, 5, 7, 9];
+
+const testBin = binarySearch(myList, 3);
+
+console.log(testBin);
 
 
 const cloneDeep = (data) => {
