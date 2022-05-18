8e72177b0f9364e5f7b154a706180ab6b5e0e34e
ElKlaus <klaus15rus@gmail.com>
2022-05-18 10:16:37 +0300

    Tree changer test

--- testjs_workspace/main.js
+++ testjs_workspace/main.js
@@ -1,3 +1,21 @@
+const changeOwner = (tree, owner) => {
+  const name = getName(tree);
+  const newMeta = _.cloneDeep(getMeta(tree));
+  newMeta.owner = owner;
+
+  if (isFile(tree)) {
+    return mkfile(name, newMeta);
+  }
+
+  const children = getChildren(tree);
+
+  const newChildren = children.map((child) => changeOwner(child, owner));
+  const newTree = mkdir(name, newChildren, newMeta);
+
+  return newTree;
+};
+
+
 const reduceTags = (coll, func, acc) => {
   if(isEmpty(coll)) {
     return acc;
