f7b591226f0b185460ef9f2fde3b74ff94a1b417
ElKlaus <klaus15rus@gmail.com>
2022-06-10 10:08:23 +0300

    setState test

--- testjs_workspace/react_lrn.jsx
+++ testjs_workspace/react_lrn.jsx
@@ -1,5 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import cn from 'classnames';
+
+
+
+export default class Buttons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: null
+    };
+  }
+
+  selectLeft = () => this.setActive('left');
+  
+  selectRight = () => this.setActive('right');
+
+  setActive = (active) => {
+    this.setState({ active });
+  }
+
+  render() {
+    const { active } = this.state;
+
+    const sharedClasses = {
+      btn: true,
+      'btn-secondary': true,
+    };
+
+    const leftButtonClass = {
+      ...sharedClasses,
+      left: true,
+      active: active === 'left',
+    };
+
+    const rightButtonClass = {
+      ...sharedClasses,
+      right: true,
+      active: active === 'right',
+    };
+
+    return (
+      <div className="btn-gtoup" role="group">
+        <button type='button' onClick={this.selectLeft} className={cn(leftButtonClass)}></button>
+        <button type='button' onClick={this.selectRight} className={cn(rightButtonClass)}></button>
+      </div>
+    )
+  }
+};
 
 class Counter3 extends React.Component {
   constructor(props) {
@@ -56,7 +104,7 @@ class Counter2 extends React.Component {
   }
 }
 
-const 
+
 
 class Clock extends React.Component {
   constructor(props) {
