b2364539f83318a2249a86bab681917316204df1
ElKlaus <klaus15rus@gmail.com>
2022-07-12 20:28:43 +0300

    Learn decorators

--- testjs_workspace/main.js
+++ testjs_workspace/main.js
@@ -12,6 +12,43 @@
  * представлений
  * приложений
  */
+const testFunc = () => {
+  let worker = {
+    someMethod: () => {
+      return 1;
+    },
+  
+    slow(x) {
+      // здесь может быть страшно тяжёлая задача для процессора
+      alert("Called with " + x);
+      return x * someMethod(); // (*)
+    }
+  };
+  
+  // тот же код, что и выше
+  function cachingDecorator(func) {
+    let cache = new Map();
+    return function(x) {
+      if (cache.has(x)) {
+        return cache.get(x);
+      }
+      let result = func(x); // (**)
+      cache.set(x, result);
+      return result;
+    };
+  }
+  
+  // alert( worker.slow(1) ); // оригинальный метод работает
+  
+  // worker.slow = cachingDecorator(worker.slow); // теперь сделаем его кеширующим
+  
+  // alert( worker.slow(2) ); // Ой! Ошибка: не удаётся прочитать свойство 'someMethod' из 'undefined'
+  
+  let func = worker.slow;
+  func(2);
+};
+
+testFunc();
 
 const getValue7 = (data, keys) => {
   let current = data;
@@ -43,7 +80,7 @@ const carsObj2 = {
   hasWings: false,
 };
 
-console.log(getValue7(carsObj2, ['platform', 'f_body', 'years']));
+// console.log(getValue7(carsObj2, ['platform', 'f_body', 'years']));
 
 const binarySearch6 = (list, item) => {
   let low = 0;
