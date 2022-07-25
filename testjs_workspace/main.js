a1e98757067aa2f48ded63f520fa1b54d5eb69c9
ElKlaus <klaus15rus@gmail.com>
2022-07-25 15:30:10 +0300

    Add cashingDecorator

--- testjs_workspace/main.js
+++ testjs_workspace/main.js
@@ -8,6 +8,40 @@
  * приложений
  * 
  */
+let worker = {
+  slow(min, max) {
+    alert(`Called with ${min}, ${max}`);
+
+    return min + max;
+  }
+};
+
+function cashingDecorator(func, hash) {
+  let cashe = new Map();
+
+  return function() {
+    let key = hash(arguments);
+
+    if (cashe.has(key)) {
+      return cashe.get(key);
+    }
+
+    let result = func.call(this, ...arguments);
+
+    cashe.set(key, result);
+
+    return result;
+  };
+}
+
+function hash(args) {
+  return args[0] + ',' + args[1];
+}
+
+worker.slow = cashingDecorator(worker.slow, hash);
+
+alert( worker.slow(3, 5));
+
 
 const objectCreate = (arg) => {
   if (!arg) {
